0d9df8490dbc7611f79f51c36a8802240fd4c914
mausam.kukreja <mausam.kukreja@gmail.com>
2018-01-07 19:30:13 +0530

    debugging envrioment name

--- script/deploy-ghpages.sh
+++ script/deploy-ghpages.sh
@@ -3,19 +3,21 @@ set -e
 # show where we are on the machine
 pwd
 
-remote=$(git config remote.origin.url)
+remote="https://$2:$4@github.com/$2/$3.git"
 
 echo "$1"
 echo "$2"
 echo "$3"
+echo "$4"
+echo "${remote}"
 # make a directory to put the gp-pages branch
 mkdir gh-pages-branch
 cd gh-pages-branch
 # now lets setup a new repo so we can update the gh-pages branch
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name "$GH_NAME" > /dev/null 2>&1
+git config --global user.email "$1" > /dev/null 2>&1
+git config --global user.name "$2" > /dev/null 2>&1
 git init > /dev/null 2>&1
-yes | git remote add --fetch origin "${remote}" > /dev/null 2>&1
+git remote add --fetch origin "${remote}" > /dev/null 2>&1
 
 # switch into the the gh-pages branch
 if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
